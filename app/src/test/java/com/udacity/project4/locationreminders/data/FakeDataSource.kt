29b58a1432616370707c336d7f1940f23a63af3d
3abcarino <eng.marconi@gmail.com>
2022-10-05 15:33:57 +0200

    error handling

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,13 +4,9 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource() :
-    ReminderDataSource {
-    var reminders: MutableList<ReminderDTO> = mutableListOf()
+class FakeDataSource() : ReminderDataSource {
 
-    override suspend fun saveReminder(reminder: ReminderDTO) {
-        reminders.add(reminder)
-    }
+    var reminders: MutableList<ReminderDTO> = mutableListOf()
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
         reminders.let {
@@ -19,7 +15,6 @@ class FakeDataSource() :
         return Result.Error("Reminders not found")
     }
 
-
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
         val reminder = reminders.find { x -> x.id == id }
         reminder?.let {
@@ -27,6 +22,11 @@ class FakeDataSource() :
         }
         return Result.Error("Reminder not found")
     }
+
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        reminders.add(reminder)
+    }
+
 
     override suspend fun deleteAllReminders() {
         reminders.clear()
