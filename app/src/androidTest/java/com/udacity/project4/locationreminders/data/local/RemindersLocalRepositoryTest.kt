66fc89bd2a12014755167264f81ed9b07141b23b
3abcarino <eng.marconi@gmail.com>
2022-10-23 10:58:50 +0200

    fix RemindersLocalRepositoryTest.kt

--- app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
+++ app/src/androidTest/java/com/udacity/project4/locationreminders/data/local/RemindersLocalRepositoryTest.kt
@@ -5,11 +5,10 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
+import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.instanceOf
 import org.hamcrest.MatcherAssert.assertThat
@@ -23,14 +22,39 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 //Medium Test to test the repository
 @MediumTest
-class RemindersLocalRepositoryTest {
+class RemindersLocalRepositoryTest(
+    private val remindersDao: RemindersDao,
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ReminderDataSource {
 
-    /*private lateinit var reminderDao: RemindersDao
-    private lateinit var remindersRepository : RemindersLocalRepository
+    override suspend fun getReminders(): Result<List<ReminderDTO>> = withContext(ioDispatcher) {
+        try {
+            val result = remindersDao.getReminders()
+            return@withContext Result.Success(result.toList())
+        } catch (e: Exception) {
+            return@withContext Result.Error(e.message)
+        }
+    }
 
-    @Before
-    fun createRepository() {
-        reminderDao = RemindersDatabase.reminderDao()
-        remindersRepository = RemindersLocalRepository(reminderDao, Dispatchers.Unconfined)
-    }*/
+    override suspend fun saveReminder(reminder: ReminderDTO) {
+        remindersDao.saveReminder(reminder)
+    }
+
+    override suspend fun getReminder(id: String): Result<ReminderDTO> = withContext(ioDispatcher) {
+        try {
+            val reminder = remindersDao.getReminderById(id)
+            if (reminder != null) {
+                val result = remindersDao.getReminderById(id)
+                return@withContext Result.Success(result!!)
+            } else {
+                return@withContext Result.Error("Reminder not found!")
+            }
+        } catch (e: Exception) {
+            return@withContext Result.Error(e.message)
+        }
+    }
+
+    override suspend fun deleteAllReminders() {
+        remindersDao.deleteAllReminders()
+    }
 }
